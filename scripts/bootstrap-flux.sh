bf7535a7255cf9ca6d40fa4b6a94def584cd4c0d
Aria <amini5454@gmail.com>
2025-06-16 01:28:18 -0400

    Fix

--- scripts/bootstrap-flux.sh
+++
@@ -1,3 +0,0 @@
-export GITHUB_TOKEN=$(cat ~/.github-token)
-flux bootstrap github --token-auth --owner=aamini11 --repository=imdbgraph-infra --branch=main --path=kubernetes/clusters/prod --personal
-flux bootstrap github --token-auth --owner=aamini11 --repository=imdbgraph-infra --branch=staging --path=kubernetes/clusters/staging --personal
--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -3,6 +3,10 @@ az aks get-credentials \
     --resource-group rg-imdbgraph-staging \
     --name aks-imdbgraph \
     --overwrite-existing
+
+FILE_DIR=$(dirname "$0")
+cd $FILE_DIR/../kubernetes/apps/imdbgraph-api/staging
+echo $(pwd)
 # Encrypt .env -> sealed-secret.yaml
 kubectl -n imdbgraph create secret generic imdbgraph-secrets \
     --from-env-file=.env \
