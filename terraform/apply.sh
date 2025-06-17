9bf7e4c89125e1e45fdacc5d461c2913c5f87cee
Aria <amini5454@gmail.com>
2025-06-16 22:59:09 -0400

    Fix

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -1,8 +1,8 @@
 FILE_DIR=$(dirname "$0")
 
-cd $FILE_DIR/..
+cd $FILE_DIR
 source .env
 export TF_VAR_github_token=$GITHUB_TOKEN
-cd ./terraform/deployments/staging
+cd ./deployments/staging
 terraform init
 terraform apply -auto-approve
